893bc7bec081558c08c7dc304d816731fbaec6a1
b.flipped <b.flipped@gmail.com>
2017-01-25 12:33:33 +0800

    not related files

---
+++ Bu_batch/build.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 
+set filepath="src\bflipped.cpp"
+set objpath ="c++primer\Bu_obj"    
+set compilerflags=/Od /Z7 /Fo"Bu_obj\\" /EHsc /I"D:\c++primer\test\include"
+set linkerflags=/OUT:Bu_out/bflipped.exe
+cl.exe %compilerflags% %filepath%  /link %linkerflags%
